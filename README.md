6d57385f796219139c169f498427321aec20d298
Ricardo Pereira <ricardoljp@gmail.com>
2020-08-20 15:33:56 -0300

    removing node_modules

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# NLWDiscovery
-Rocketseat Next Level Challenge - Discovery
+<h1 align="center">
+    <img alt="Proffy" title="Proffy" src=".github/logo.svg" />
+</h1>
 
-I followed up a week challenge development to update my skills using HTML and CSS.
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
 
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=8257E5&labelColor=000000" alt="PRs welcome!" />
 
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Proffy" src=".github/proffy.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/pt-br/)
+- [SQLite](https://www.sqlite.org/index.html)
+- [Nunjucks](https://mozilla.github.io/nunjucks/)
+
+## 💻 Projeto
+
+O Proffy é uma plataforma de estudos online que ajuda pessoas a encontrarem professores online.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [desse link](https://www.figma.com/file/GHGS126t7WYjnPZdRKChJF/Proffy-Web). Lembrando que você precisa ter uma conta no [Figma](http://figma.com/) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by Rocketseat :wave: [Participe da nossa comunidade!](https://discordapp.com/invite/gCRAFhc)
--- src/server.js
+++ src/server.js
@@ -1,99 +1,32 @@
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
-        whatsapp: "89898898988989898" ,
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões." , 
-        subject: "Química", 
-        cost: 20.00, 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    },
-    {
-        name: "Diego Bolinha",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
-        whatsapp: "89898898988989898" ,
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões." , 
-        subject: "Química", 
-        cost: 20.00, 
-        weekday: [1], 
-        time_from: [], 
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-        "Artes",
-        "Biologia",
-        "Ciências",
-        "Educação Física",
-        "Geografia",
-        "História",
-        "Matemática",
-        "Português",
-        "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber -1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isnotEmpty = Object.keys(data).length > 0
-    //if data is filled then add data to proffys object
-    if (isnotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    } 
-    
-    //else show the page
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
-
+// Servidor
 const express = require('express')
 const server = express()
 
-//nunjucks configuration
+const  {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+
+//configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
-nunjucks.configure('src/views', { 
+nunjucks.configure('src/views', {
     express: server,
-    noCache: true
+    noCache: true,
 })
 
+// Inicio e configuração do servidor
 server
-//Configuration for static files (css, scripts, images)
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+// configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
-//app routes
+// rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-
+.post("/save-classes", saveClasses)
+// start do servidor
 .listen(5500)
